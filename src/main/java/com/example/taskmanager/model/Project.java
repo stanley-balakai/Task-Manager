a3c6b6fb4a553914914a07b1bd17ef670a4db9b1
Stanley Balakai <sbalakai@gmail.com>
2023-04-09 18:31:49 -0400

    first commit

---
+++ src/main/java/com/example/taskmanager/model/Project.java
@@ -0,0 +1,34 @@
+package com.example.taskmanager.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    // Constructors, getters and setters
+}
+
---
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -0,0 +1,39 @@
+package com.example.taskmanager.model;
+
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @SuppressWarnings("unused")
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    public Task() {
+    }
+
+    // Getters and setters for the fields
+}
+

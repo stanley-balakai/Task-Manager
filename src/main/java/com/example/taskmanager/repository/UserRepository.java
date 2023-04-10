a6276c5c1ad30fa4daaf25bbc69b9d6b61459caf
Stanley Balakai <sbalakai@gmail.com>
2023-04-10 17:56:41 -0400

    filled repository folder

---
+++ src/main/java/com/example/taskmanager/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.taskmanager.repository;
+
+import com.example.taskmanager.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
